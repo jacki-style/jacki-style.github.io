c01a8019d55a36f47815c78ebb92045857e73b60
Michelle Tran Luu <miss_amtl@live.com>
2018-08-12 17:58:48 +0200

    added mail-function

--- script.js
+++ script.js
@@ -7,24 +7,15 @@ $(document).ready(function(){
 return false;
   });
 
-  // /* Every time the window is scrolled ... */
-  //   $(window).scroll( function(){
-  //
-  //       /* Check the location of each desired element */
-  //       $('.hideme').each( function(i){
-  //
-  //           var bottom_of_object = $(this).offset().top + $(this).outerHeight()/2;
-  //           var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //
-  //           /* If the object is completely visible in the window, fade it it */
-  //           if( bottom_of_window > bottom_of_object ){
-  //
-  //               $(this).animate({'opacity':'1'},500);
-  //
-  //           }
-  //
-  //       });
-  //
-  //   });
+  $("#sign-up").on("click", function(){
+    email = $("#email").val();
+
+    $.post('https://jacki-backend.herokuapp.com/register?email=' + email, function(data) {
+      if (data.result == "ok") {
+        $('.join').css('display', 'none');
+        $('.thanks-newsletter').css('display', 'flex');
+      }
+    });
+  });
 
 });
