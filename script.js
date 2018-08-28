a91fb9c3cccd76bc5bcded39e94c545199c71983
Michelle Tran Luu <miss_amtl@live.com>
2018-08-28 23:11:30 +0900

    added empty email-validation

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ return false;
 
   $("#sign-up").on("click", function(){
     email = $("#email").val();
-
-    $.post('https://jacki-backend.herokuapp.com/register?email=' + email, function(data) {
-      if (data.result == "ok") {
-        $('.join').css('display', 'none');
-        $('.thanks-newsletter').css('display', 'flex');
-      }
-    });
+    if (email != "") {
+      $('.join').css('display', 'none');
+      $('.thanks-newsletter').css('display', 'flex');
+      $.post('https://jacki-backend.herokuapp.com/register?email=' + email, function(data) {
+      });
+    }
   });
 
 });
