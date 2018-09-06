7db20e56d6db1b849660dfd72a7315ca512034fb
Michelle Tran Luu <miss_amtl@live.com>
2018-09-06 08:06:13 +0200

    added checkbox for emailaddress and a footer

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
 
   $("#header-btn").on("click", function(){
     $("html, body, .main").animate({
-     scrollTop: $(".sign-up-section").offset().top}, 1000);
+     scrollTop: $(".sign-up-section").offset().top+80}, 1000);
 return false;
   });
 
   $("#sign-up").on("click", function(){
     email = $("#email").val();
-    if (email != "") {
+    terms = $("#terms").prop("checked");
+
+    if (terms && email != "") {
       $('.join').css('display', 'none');
       $('.thanks-newsletter').css('display', 'flex');
       $.post('https://jacki-backend.herokuapp.com/register?email=' + email, function(data) {
