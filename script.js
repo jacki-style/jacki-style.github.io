8bcf69c7d4a57d543cc7a04617f8527a8956bd54
Michelle Tran Luu <miss_amtl@live.com>
2018-12-21 17:10:38 +0100

    added more pages

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ $(document).ready(function(){
   $("#header-btn").on("click", function(){
     $("html, body, .main").animate({
      scrollTop: $(".sign-up-section").offset().top+80}, 1000);
-return false;
+     return false;
+  });
+
+  $(".menu-hamburger").on("click", function(){
+    $(".menu-hamburger").toggleClass('active');
+    $(".fly-in-menu").toggleClass('active');
+    $(".menu").toggleClass('burger-active');
+  });
+
+  $(".fly-in-menu").on("click", function(e){
+    if (e.target !== this){
+      return;
+    }
+    $(".menu").removeClass('burger-active');
+    $(".menu-hamburger").removeClass('active');
+    $(".fly-in-menu").removeClass('active');
   });
 
   $("#sign-up").on("click", function(){
@@ -20,4 +35,9 @@ return false;
     }
   });
 
+  $(".FAQ-short").on("click", function(){
+    idSection = $(this).attr('id');
+    $("html, body, .main").animate({
+     scrollTop: $("#FAQ-"+idSection).offset().top}, 2000);
+  })
 });
