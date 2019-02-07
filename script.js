34e615f2e645ef3dcd4e4e0446785011b17db254
Michelle Tran Luu <miss_amtl@live.com>
2019-02-07 22:22:15 +0100

    added error-handling

--- script.js
+++ script.js
@@ -22,15 +22,30 @@ $(document).ready(function(){
     $(".fly-in-menu").removeClass('active');
   });
 
+  $("#email").focusout(function (){
+    if(!isValidEmailAddress($("#email").val())){
+      $(".label-email").css('top', 'calc(50% - 92px)')
+      $(".error-email").show()
+    } else {
+      $(".label-email").css('top', 'calc(50% - 70px)')
+      $(".error-email").hide()
+    }
+  });
+
   $("#sign-up").on("click", function(){
     email = $("#email").val();
     terms = $("#terms").prop("checked");
 
-    if (terms && email != "") {
-      $('.join').css('display', 'none');
-      $('.thanks-newsletter').css('display', 'flex');
+    if (terms && isValidEmailAddress(email)) {
       $.post('https://jacki-backend.herokuapp.com/register?email=' + email, function(data) {
-      });
+      })
+        .done(function() {
+          $('.join').css('display', 'none');
+          $('.thanks-newsletter').css('display', 'flex');
+        })
+        .fail(function() {
+          $(".error-response").show()
+        })
       fbq('track', 'CompleteRegistration');
     }
   });
@@ -41,3 +56,8 @@ $(document).ready(function(){
      scrollTop: $("#FAQ-"+idSection).offset().top}, 2000);
   })
 });
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
